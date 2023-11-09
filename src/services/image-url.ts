0321274823fd45615ee07a4246a191966127da22
Ailin Garcia <agarcia@switch.com.uy>
2023-11-09 16:30:23 -0300

    handling empty images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
 export const getCroppedImageUrl = (url: string) => {
+    if (!url) return ''
+    
     const locator = 'media/'
     const index = url.indexOf(locator) + locator.length
     return `${url.slice(0, index)}crop/600/400/${url.slice(index)}`
